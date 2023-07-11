308a9738ea8edfebda3df9376cd95b7c9c905dbb
Wilmer Vasquez De Leon <cort-1998@hotmail.com>
2023-07-11 05:42:05 -0400

    Add project files.

---
+++ MSL.Domain/Entities/Astromon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSL.Domain.Entities
+{
+    public class Astromon
+    {
+        public Guid GuidId { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
